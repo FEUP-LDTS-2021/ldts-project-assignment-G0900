f20bd73cdf713004a8b411171f1ed494513ef74b
Marco <marco14lyoko@gmail.com>
2022-01-01 23:28:05 +0000

    Added Block Class that inherits from Element

--- src/main/java/Coin.java
+++ src/main/java/Coin.java
@@ -1,8 +1,3 @@
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.TextGraphics;
-
 public class Coin extends Element {
 
     public Coin(int x, int y, Character character, String color) {
