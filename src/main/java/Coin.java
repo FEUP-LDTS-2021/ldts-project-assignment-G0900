27128e3671963d5cbab435471f555b18094e02ce
Marco <marco14lyoko@gmail.com>
2022-01-01 23:09:38 +0000

    Added a new Coin Class that inherits from Element

---
+++ src/main/java/Coin.java
@@ -0,0 +1,14 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Coin extends Element {
+
+    public Coin(int x, int y, Character character, String color) {
+        super(x, y, character, color);
+    }
+
+    //More to add...
+
+}
